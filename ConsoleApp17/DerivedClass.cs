c809d915bbc26b360c465d68334edef2ad606772
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-04-17 11:15:56 +0300

    Add files via upload

---
+++ ConsoleApp17/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class DerivedClass:IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод, реализованный в классе DerivedClass");
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method();
+            Console.Write("");
+            IInterface myInterface = dClass;
+            myInterface.Method();
+            Console.Read();
+        }
+    }
+}
